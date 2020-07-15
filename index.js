504858ad934099c80d9e52ee472133d768819e6c
rkomiyama <ryukomiyama@gmail.com>
2020-07-15 18:45:56 -0400

    Adds create/delete/list commands

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const Telegraf = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.command("hello", ctx => ctx.reply("Hello from AWS!!!"));
+bot.command("list", ctx => ctx.reply("List of events"));
+bot.command("create", ctx => ctx.reply("Creates event"));
+bot.command("delete", ctx => ctx.reply("Deletes event"));
 
 exports.handler = (event, context, callback) => {
   const tmp = JSON.parse(event.body);
